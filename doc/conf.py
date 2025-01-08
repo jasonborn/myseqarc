656e2d4941b655936189280bb03d1811371735e3
code0&1 <zijian0818@163.com>
2025-01-08 16:00:01 +0800

    update reaeme

--- doc/conf.py
+++ doc/conf.py
@@ -1,68 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('../gefpy'))
-sys.path.insert(0, os.path.abspath('..'))
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SeqArc'
-copyright = '2022, zhaozijian'
+copyright = '2025, zhaozijian'
 author = 'zhaozijian'
-
-# The full version, including alpha/beta/rc tags
 release = '3.0.0'
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    #'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    "sphinx_autodoc_typehints",
-    "sphinx_rtd_theme",
-]
-#
-# Add any paths that contain templates here, relative to this directory.
+extensions = []
+
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# Generate the API documentation when building
-autosummary_generate = True
-autodoc_member_order = 'bysource'
-autodoc_default_flags = ['members']
-todo_include_todos = False
-#api_dir = HERE / 'api'  # function_images
-# The master toctree document.
-language = 'en'
+language = 'y'
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['static']
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_css_files = [
-    'html.css',
-]
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. SeqArc documentation master file, created by
+   sphinx-quickstart on Wed Jan  8 15:49:53 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SeqArc's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
